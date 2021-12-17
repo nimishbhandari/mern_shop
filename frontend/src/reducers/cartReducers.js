90aaba375eb31d1c8c3381e96a241b94eec99545
Nimish Bhandari <nimishbhandari727521@gmail.com>
2021-12-17 18:30:56 +0530

    shipping screen

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,6 +1,13 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS,
+} from "../constants/cartConstants";
 
-export const cartReducers = (state = { cartItems: [] }, action) => {
+export const cartReducers = (
+  state = { cartItems: [], shippingAddress: {} },
+  action
+) => {
   switch (action.type) {
     case CART_ADD_ITEM:
       const item = action.payload;
@@ -25,6 +32,12 @@ export const cartReducers = (state = { cartItems: [] }, action) => {
       return {
         ...state,
         cartItems: state.cartItems.filter((x) => x.product !== action.payload),
+      };
+
+    case CART_SAVE_SHIPPING_ADDRESS:
+      return {
+        ...state,
+        shippingAddress: action.payload,
       };
 
     default:
--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -1,7 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
+import FormContainer from "../components/FormContainer";
+import { Button, Form } from "react-bootstrap";
+import { useSelector, useDispatch } from "react-redux";
+import { saveShippingAddress } from "../actions/cartActions";
+import { useNavigate } from "react-router-dom";
 
 const ShippingScreen = () => {
-  return <div>shipping</div>;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+
+  const [address, setAddress] = useState(shippingAddress.address);
+  const [city, setCity] = useState(shippingAddress.city);
+  const [postalCode, setPostalCode] = useState(shippingAddress.postalCode);
+  const [country, setCountry] = useState(shippingAddress.country);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("yes");
+    dispatch(saveShippingAddress({ address, city, postalCode, country }));
+    navigate("/payment");
+  };
+
+  return (
+    <div>
+      <FormContainer>
+        <h1>Shipping</h1>
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId="address" className="my-3">
+            <Form.Label>Address </Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter address"
+              value={address || ""}
+              required
+              onChange={(e) => setAddress(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId="city" className="my-3">
+            <Form.Label>City </Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter City"
+              value={city || ""}
+              required
+              onChange={(e) => setCity(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId="Postal Code" className="my-3">
+            <Form.Label>Postal Code </Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Postal Code"
+              value={postalCode || ""}
+              required
+              onChange={(e) => setPostalCode(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId="country" className="my-3">
+            <Form.Label>Country </Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter country"
+              value={country || ""}
+              required
+              onChange={(e) => setCountry(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+
+          <Button className="my-3" type="submit" variant="success">
+            Continue
+          </Button>
+        </Form>
+      </FormContainer>
+    </div>
+  );
 };
 
 export default ShippingScreen;
