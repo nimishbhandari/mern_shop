fc198b7c302e048168fe3bd9e067c1f88afa5d1d
Nimish Bhandari <nimishbhandari727521@gmail.com>
2021-12-21 19:25:56 +0530

    place order screen

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -12,6 +12,16 @@ const PlaceOrderScreen = () => {
     .reduce((acc, item) => acc + item.price * item.qty, 0)
     .toFixed(2);
 
+  cart.shippingPrice = cart.itemsPrice > 100 ? 0 : 100;
+
+  cart.taxPrice = Number((0.15 * cart.itemsPrice).toFixed(2));
+
+  cart.totalPrice = (
+    Number(cart.itemsPrice) +
+    Number(cart.shippingPrice) +
+    Number(cart.taxPrice)
+  ).toFixed(2);
+
   const placeOrderHandler = () => {
     console.log("Order Placed");
   };
