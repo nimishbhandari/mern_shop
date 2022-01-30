cbd64d65da18f2fbfd6296867c7596082d83f3f1
Nimish Bhandari <nimishbhandari727521@gmail.com>
2022-01-30 20:00:36 +0530

    .

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
 import {
   authUser,
+  deleteUser,
+  getUserById,
   getUserProfile,
   getUsers,
   registerUser,
+  updateUser,
   updateUserProfile,
 } from "../controllers/userController.js";
 import { admin, protect } from "../middleware/authMiddleware.js";
@@ -17,5 +20,11 @@ router
   .route("/profile")
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
+
+router
+  .route("/:id")
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
 
 export default router;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
 import UserListScreen from "./screens/UserListScreen";
+import UserEditScreen from "./screens/UserEditScreen";
 
 const App = () => {
   return (
@@ -35,6 +36,7 @@ const App = () => {
             <Route path={"/placeorder"} element={<PlaceOrderScreen />} />
             <Route path={"/order/:id"} element={<OrderScreen />} />
             <Route path={"/admin/userlist"} element={<UserListScreen />} />
+            <Route path="/admin/user/:id/edit" element={<UserEditScreen />} />
           </Routes>
         </Container>
       </main>
